2415f200337cd4d4a509fbb89b38e199b0401b33
Labesh Baral <99603007+labeshbaral1@users.noreply.github.com>
2023-03-04 18:11:27 -0500

    wokring ripe/test

---
+++ routes/ripe.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const TeachableMachine = require("@sashido/teachablemachine-node");
+
+const model = new TeachableMachine({
+  modelUrl: "https://teachablemachine.withgoogle.com/models/N58PlX_GN/"
+});
+
+
+router.get("/image/classify", async (req, res) => {
+    const { url } = req.query;
+  
+
+    const getHighestScoreClass = (predictions) => {
+    const sortedPredictions = predictions.sort((a, b) => b.score - a.score);
+    return sortedPredictions[0].class;
+  }
+  
+  
+    return model.classify({
+      imageUrl: url,
+    }).then((predictions) => {
+      const highestScoreClass = getHighestScoreClass(predictions);  
+      return res.json({classification: highestScoreClass});
+    }).catch((e) => {
+      console.error(e);
+      res.status(500).send("Something went wrong!")
+    });
+  });
+  
+
+
+  module.exports = router;
+
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/users');
+const ripeRouter = require('./routes/ripe')
 
 dotenv.config();
 app.use(express.json());
+app.use(cors()); // Use the cors middleware
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -20,10 +23,13 @@ db.once('open', () => console.log('Connected to MongoDB'));
 db.on('error', (err) => console.error('connection error:', err));
 
 app.use('/users', userRouter);
+app.use('/ripe', ripeRouter);
+
 app.use('*', (req, res) => {
-	res.status(404).json({ message: '404 Page Not Found' });
+  res.status(404).json({ message: '404 Page Not Found' });
 });
 
+
 app.listen(process.env.PORT || 3666, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });

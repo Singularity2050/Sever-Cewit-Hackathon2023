fa4a83a8f15c206f34e2e86931ee03df0e30a447
Seungjun <seungjun.chae@stonybrook.edu>
2023-03-04 21:52:44 -0500

    removed one cors

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const cors = require('cors'); // Import the cors middleware
-const app = express();
 const cors = require('cors');
+const app = express();
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/users');
-const ripeRouter = require('./routes/ripe')
+const ripeRouter = require('./routes/ripe');
 
 dotenv.config();
 app.use(express.json());
@@ -14,8 +13,8 @@ app.use(cors()); // Use the cors middleware
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -27,10 +26,9 @@ app.use('/users', userRouter);
 app.use('/ripe', ripeRouter);
 
 app.use('*', (req, res) => {
-  res.status(404).json({ message: '404 Page Not Found' });
+	res.status(404).json({ message: '404 Page Not Found' });
 });
 
-
 app.listen(process.env.PORT || 3666, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
